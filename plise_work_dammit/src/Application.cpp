4a178c9c526be10d8a819adea3a1c082830c5883
Samuel Martel <samuel_martel@anciens-mss.ca>
2019-04-18 13:57:34 -0400

    Vertex Attributes and Layouts

--- plise_work_dammit/src/Application.cpp
+++ plise_work_dammit/src/Application.cpp
@@ -1,4 +1,11 @@
+// http://docs.gl/ 
+// OpenGl Documentation.
+
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+
 
 int main( void ) {
     GLFWwindow* window;
@@ -17,16 +24,35 @@ int main( void ) {
     /* Make the window's context current */
     glfwMakeContextCurrent( window );
 
+    if ( glewInit() != GLEW_OK )
+        std::cout << "Error at glewInit!" << std::endl;
+
+    std::cout << "Using OpenGL Version " << glGetString( GL_VERSION ) << std::endl;
+
+    // Create an openGL buffer.
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers( 1, &buffer );
+    glBindBuffer( GL_ARRAY_BUFFER, buffer );
+    glBufferData( GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW );
+
+
+    glEnableVertexAttribArray( 0 );
+    glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( float ) * 2, 0 );
+
+    glBindBuffer( GL_ARRAY_BUFFER, 0 );
     /* Loop until the user closes the window */
     while ( !glfwWindowShouldClose( window ) ) {
         /* Render here */
         glClear( GL_COLOR_BUFFER_BIT );
 
-        glBegin( GL_TRIANGLES );
-        glVertex2f( -0.5f, -0.5f );
-        glVertex2f( 0.0f,  0.5f );
-        glVertex2f( 0.5f, -0.5f );
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
     
         /* Swap front and back buffers */
         glfwSwapBuffers( window );
