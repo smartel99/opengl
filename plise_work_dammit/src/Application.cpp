0505f913e80e309f072ada10c921a19a52d6464b
Raldryniorth <martelsamuel00@gmail.com>
2019-04-21 09:36:09 -0400

    Shaders are into a file! Also I'm feeling a lot better than last commit for anyone that read those :)

--- plise_work_dammit/src/Application.cpp
+++ plise_work_dammit/src/Application.cpp
@@ -5,7 +5,16 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
 
+struct ShaderProgramSource {
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string&);
 static unsigned int CompileShader(unsigned int type, const std::string& source);
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 
@@ -47,28 +56,10 @@ int main( void ) {
     glEnableVertexAttribArray( 0 );
     glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( float ) * 2, 0 );
 
-	std::string vertexShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	gl_Position = position;\n"
-		"}\n";
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-	std::string fragmentShader =
-		"#version 330 core\n"
-		"\n"
-		"layout(location = 0) out vec4 color;"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n" 
-		"}\n";
-
-	unsigned int shader = CreateShader(vertexShader, fragmentShader);
-	glUseProgram(shader);
+	/*unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);*/
 
     glBindBuffer( GL_ARRAY_BUFFER, 0 );
     /* Loop until the user closes the window */
@@ -86,10 +77,40 @@ int main( void ) {
         glfwPollEvents();
     }
 
-	glDeleteProgram(shader);
+	//glDeleteProgram(shader);
     
 	glfwTerminate();
     return 0;
+}
+
+ShaderProgramSource ParseShader(const std::string& filepath)
+{
+	std::ifstream stream(filepath);
+
+	enum class ShaderType {
+		NONE = -1, VERTEX = 0, FRAGMENT = 1 
+	};
+
+	std::string line;
+	std::stringstream ss[2];
+
+	ShaderType type = ShaderType::NONE;
+
+	while (getline(stream, line)) {
+		if (line.find("shader") != std::string::npos) {
+			if (line.find("vertex") != std::string::npos) {
+				type = ShaderType::VERTEX;
+			}
+			else if (line.find("fragment") != std::string::npos) {
+				type = ShaderType::FRAGMENT;
+			}
+		}
+		else {
+			ss[(int)type] << line << '\n';
+		}
+	}
+
+	return { ss[0].str(), ss[1].str() };
 }
 
 unsigned int CompileShader(unsigned int type, const std::string& source)
