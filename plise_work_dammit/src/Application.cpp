12988559a35fd66aab84fab4c409c9d48ff12253
Raldryniorth <martelsamuel00@gmail.com>
2019-04-22 18:28:15 -0400

    movee movee colory changy

--- plise_work_dammit/src/Application.cpp
+++ plise_work_dammit/src/Application.cpp
@@ -8,6 +8,7 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <cassert>
 
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
@@ -24,81 +25,135 @@ static bool GLLogCall(const char* function, const char* file, int line);
 static ShaderProgramSource ParseShader(const std::string&);
 static unsigned int CompileShader(unsigned int type, const std::string& source);
 static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+void HSVtoRGB(int H, float S, float V, float* r, float* g, float* b);
 
-int main( void ) {
-    GLFWwindow* window;
+int main(void) {
+	GLFWwindow* window;
 
-    /* Initialize the library */
-    if ( !glfwInit() )
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow( 640, 480, "Hello World", NULL, NULL );
-    if ( !window ) {
-        glfwTerminate();
-        return -1;
-    }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent( window );
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Blinky Recty", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
 
-    if ( glewInit() != GLEW_OK )
-        std::cout << "Error at glewInit!" << std::endl;
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    std::cout << "Using OpenGL Version " << glGetString( GL_VERSION ) << std::endl;
+	glfwSwapInterval(1);
 
-    // Create an openGL buffer.
-    float positions[] = {
-        -0.5f, -0.5f,	// 0
-         0.5f, -0.5f,	// 1
-         0.5f,  0.5f,	// 2
-        -0.5f,  0.5f	// 3
-    };
+	if (glewInit() != GLEW_OK)
+		std::cout << "Error at glewInit!" << std::endl;
+
+	std::cout << "Using OpenGL Version " << glGetString(GL_VERSION) << std::endl;
+
+	// Create an openGL buffer.
+	float positions[] = {
+		-0.5f, -0.5f,	// 0
+		 0.5f, -0.5f,	// 1
+		 0.5f,  0.5f,	// 2
+		-0.5f,  0.5f	// 3
+	};
 
 	// The values of this array represent the index of a vector in the position array.
 	// No matter what type that is chosen for this array, it must be unsigned.
 	unsigned int indices[] = {
 		0, 1, 2,
-		2, 3, 0 
+		2, 3, 0
 	};
 
-    unsigned int buffer;
-	GLCall(glGenBuffers( 1, &buffer ));
-	GLCall(glBindBuffer( GL_ARRAY_BUFFER, buffer ));
-	GLCall(glBufferData( GL_ARRAY_BUFFER, 6 * 2 *sizeof(float), positions, GL_STATIC_DRAW ));
+	unsigned int vao;	// Vertex Array Buffer.
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
 
-	GLCall(glEnableVertexAttribArray( 0 ));
-	GLCall(glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof( float ) * 2, 0 ));
+	unsigned int buffer;
+	GLCall(glGenBuffers(1, &buffer));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_DYNAMIC_DRAW));
+
+	GLCall(glEnableVertexAttribArray(0));
+	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 
 	unsigned int ibo;	// ibo -> index buffer object.
 	GLCall(glGenBuffers(1, &ibo));
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
 
-	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");	
+	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	GLCall(glUseProgram(shader));
 
-	GLCall(glBindBuffer( GL_ARRAY_BUFFER, 0 ));
-    /* Loop until the user closes the window */
-    while ( !glfwWindowShouldClose( window ) ) {
-        /* Render here */
-        glClear( GL_COLOR_BUFFER_BIT );
+	GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
 
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
+	float sat = 1.0f;	// HSV Saturation.
+	int hue = 0;		// HSV Hue.
+
+	float r = 0.0f;
+	float g = 0.0f;
+	float b = 0.0f;
+	float a = 1.0f;
+	float increment = 0.01f;
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window)) {
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		GLCall(glUseProgram(shader));
+		GLCall(glUniform4f(location, r, g, b, a));
+
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+		GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_DYNAMIC_DRAW));
+		
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
-    
-        /* Swap front and back buffers */
-        glfwSwapBuffers( window );
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+		if (hue > 255)
+			hue = 0;
+
+		HSVtoRGB(hue, sat, 1.0f, &r, &g, &b);
+		hue++;
+
+		if (positions[1] > 0.0f) {
+			increment = -0.01f;
+		}
+		else if (positions[1] < -1.0f) {
+			increment = 0.01f;
+		}
+		for (int i = 0; i < 8; i++) {
+			if (i % 4) {
+				positions[i] += increment;
+			}
+			
+		}
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
 
 	glDeleteProgram(shader);
-    
+
 	glfwTerminate();
-    return 0;
+	return 0;
 }
 
 void GLClearError()
@@ -109,19 +164,19 @@ void GLClearError()
 bool GLLogCall(const char* function, const char* file, int line)
 {
 	while (GLenum error = glGetError()) {
-		std::cout << "[OpenGL Error]: <" << error << ">: " << function << 
+		std::cout << "[OpenGL Error]: <" << error << ">: " << function <<
 			" " << file << ": " << line << std::endl;
 		return false;
 	}
 	return true;
 }
 
-ShaderProgramSource ParseShader(const std::string& filepath)
+ShaderProgramSource ParseShader(const std::string & filepath)
 {
 	std::ifstream stream(filepath);
 
 	enum class ShaderType {
-		NONE = -1, VERTEX = 0, FRAGMENT = 1 
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
 	};
 
 	std::string line;
@@ -146,7 +201,7 @@ ShaderProgramSource ParseShader(const std::string& filepath)
 	return { ss[0].str(), ss[1].str() };
 }
 
-unsigned int CompileShader(unsigned int type, const std::string& source)
+unsigned int CompileShader(unsigned int type, const std::string & source)
 {
 	unsigned int id = glCreateShader(type);
 	const char* src = source.c_str(); // Equivalent to &source[0].
@@ -155,16 +210,16 @@ unsigned int CompileShader(unsigned int type, const std::string& source)
 
 	int result;
 	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-	
+
 	if (result == GL_FALSE) {
 		int length;
 		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-		char* message = (char*)alloca(length*sizeof(char));
+		char* message = (char*)alloca(length * sizeof(char));
 		glGetShaderInfoLog(id, length, &length, message);
 		std::cout << "Failed to compile " <<
 			(type == GL_VERTEX_SHADER ? "vertex" : "fregment")
 			<< " shader!" << std::endl;
-		std::cout<<message<<std::endl;
+		std::cout << message << std::endl;
 		glDeleteShader(id);
 		return 0;
 	}
@@ -172,7 +227,7 @@ unsigned int CompileShader(unsigned int type, const std::string& source)
 	return id;
 }
 
-unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+unsigned int CreateShader(const std::string & vertexShader, const std::string & fragmentShader)
 {
 	unsigned int program = glCreateProgram();
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
@@ -188,4 +243,47 @@ unsigned int CreateShader(const std::string& vertexShader, const std::string& fr
 	glDeleteShader(fs);
 
 	return program;
+}
+
+void HSVtoRGB(int H, float S, float V, float* r, float* g, float* b)
+{
+	float C = S * V;
+	float X = C * (1 - abs(fmod(H / 60.0, 2) - 1));
+	float m = V - C;
+	float Rs, Gs, Bs;
+
+	if (H >= 0 && H < 60) {
+		Rs = C;
+		Gs = X;
+		Bs = 0;
+	}
+	else if (H >= 60 && H < 120) {
+		Rs = X;
+		Gs = C;
+		Bs = 0;
+	}
+	else if (H >= 120 && H < 180) {
+		Rs = 0;
+		Gs = C;
+		Bs = X;
+	}
+	else if (H >= 180 && H < 240) {
+		Rs = 0;
+		Gs = X;
+		Bs = C;
+	}
+	else if (H >= 240 && H < 300) {
+		Rs = X;
+		Gs = 0;
+		Bs = C;
+	}
+	else {
+		Rs = C;
+		Gs = 0;
+		Bs = X;
+	}
+
+	*r = (Rs + m);
+	*g = (Gs + m);
+	*b = (Bs + m);
 }
