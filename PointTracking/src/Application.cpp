16551fba2cdf5548c45ae220770bc0361670feac
Samuel Martel <samuel_martel@anciens-mss.ca>
2019-05-07 16:07:58 -0400

    Tracking Top-Left most corner of the detected point

--- PointTracking/src/Application.cpp
+++ PointTracking/src/Application.cpp
@@ -12,14 +12,23 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/tracking.hpp>
 
 #include "librealsense2/rs.hpp"
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+
 using namespace cv;
-using namespace cv::ximgproc::segmentation;
 
-
+int GetMaxAreaContourId(std::vector<std::vector<Point>> contours);
 void PrintRealsenseError(const rs2::error& e);
+void FindBiggestContour(const Mat* src, const Mat* output, Rect inital_box, int r);
+void FindTopMostContour(const Mat* src, const Mat* output, Rect initial_box, int r);
+
+int thresh = 20;
+int thresh_multiplier = 5;
+int max_recursion = 10;
+
 
 int main(int argc, char** argv) {
 	/// --- REALSENSE INIT ---
@@ -41,6 +50,8 @@ int main(int argc, char** argv) {
 		frames = pipe.wait_for_frames();
 	}
 	rs2::video_frame vf = frames.get_color_frame();
+	Mat old_image = Mat(Size(640, 480), CV_8UC3, (void*)vf.get_data(), Mat::AUTO_STEP);
+	Mat new_image = Mat(Size(640, 480), CV_8UC3, (void*)vf.get_data(), Mat::AUTO_STEP);
 	/// --- END OF REALSENSE INIT ---
 
 	/// --- OPENCV INIT ---
@@ -48,81 +59,152 @@ int main(int argc, char** argv) {
 	setUseOptimized(true);
 	setNumThreads(12);
 
-	// Create selective search segmentation object using default parameters.
-	Ptr<SelectiveSearchSegmentation> ss = createSelectiveSearchSegmentation();
+	namedWindow("New", WINDOW_AUTOSIZE);
+	namedWindow("Thresh", WINDOW_FREERATIO);
 
-	namedWindow("Tracking", WINDOW_AUTOSIZE);
+	createTrackbar(" Threshold", "New", &thresh, 255, nullptr);
+	createTrackbar(" Threshold Multiplier", "New", &thresh_multiplier, 5, nullptr);
+	createTrackbar(" Max Recursion", "New", &max_recursion, 10, nullptr);
+
 	/// --- END OF OPENCV INIT ---
 
-	// Number of region proposals to show.
-	int numShowRects = 100;
-	// Increment to increase/decrease total number of region proposals to be shown.
-	int increment = 20;
+	int updateInterval = 0;
 
-	Mat im = Mat(Size(640, 480), CV_8UC3, (void*)vf.get_data(), Mat::AUTO_STEP);
-	ss->setBaseImage(im);
-	ss->switchToSelectiveSearchFast();
-	int newHeight = 200;
-	int newWidth = im.cols * newHeight / im.rows;
+	while (waitKey(1) == -1) {
+		if (pipe.poll_for_frames(&frames)) {
 
-	while (1) {
-
-		if (pipe.poll_for_frames(&frames))
-		{
 			// Start timer.
 			double timer = (double)getTickCount();
 
-			im = Mat(Size(640, 480), CV_8UC3, (void*)frames.get_color_frame().get_data(), Mat::AUTO_STEP);
-			// Resize image.
-			resize(im, im, Size(newWidth, newHeight));
-			ss->setBaseImage(im);
-			ss->switchToSelectiveSearchFast();
+			new_image = Mat(Size(640, 480), CV_8UC3, (void*)frames.get_color_frame().get_data(), Mat::AUTO_STEP);
 
+			// --- DETECTION ---
+			Mat diff, diff_gray;
+			Mat output = new_image.clone();
+			absdiff(new_image, old_image, diff);
 
-			// Run selective search segmentation on input image.
-			std::vector<Rect> rects;
-			ss->process(rects);
+			// Convert to gray scale and apply blur.
+			cvtColor(diff, diff_gray, COLOR_BGR2GRAY);
+			blur(diff_gray, diff_gray, Size(3, 3));
+
+			FindTopMostContour(&diff_gray, &output, Rect(0, 0, 0, 0), 0);
+
+			// --- END OF DETECTION ---
 
 			// Calculate FPS.
 			float fps = getTickFrequency() / ((double)getTickCount() - timer);
-			std::cout << "FPS: " << fps << '\r';
+			// Display FPS on frame.
+			std::ostringstream oss;
+			oss << "FPS : " << (int(fps));
+			putText(output, oss.str(), Point(100, 20), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(50, 170, 50), 2);
 
-			// Iterate over all the region proposals.
-#pragma omp parallel for schedule(dynamic)  // Using OpenMP to try to parallelise the loop.
-			for (int i = 0; i < rects.size(); i++) {
-				if (i < numShowRects) {
-					rectangle(im, rects[i], Scalar(0, 255, 0));
-				}
-				else
-					break;
-			}
-
-			// Show output.
-			imshow("Tracking", im);
-
-			// Record key press.
-			int k = waitKey(1);
-
-			if (k != -1) {
-				switch (k) {
-					// m is pressed.
-				case 109:
-					// Increase total number of rectangles to show by increment.
-					numShowRects += increment;
-					break;
-					// l is pressed
-				case 108:
-					if (numShowRects > increment)
-						// Decrease total number of rectangles to show by increment.
-						numShowRects -= increment;
-					break;
-					// q is pressed.
-				case 113:
-					return(0);
-				}
-			}
+			imshow("New", output);
+			//imshow("Thresh", cropImage);
 		}
 	}
+}
+
+void FindBiggestContour(const Mat * src, const Mat * output, Rect initial_box, int r) {
+	if ((initial_box.area() <= 10 && initial_box.area() != 0) || r >= 5) {
+		return;
+	}
+
+	std::vector<std::vector<Point>> contours;
+	std::vector<Vec4i> hierarchy;
+
+	Mat threshold_output;
+
+	// Detect edges using Threshold.
+	threshold(*src, threshold_output, thresh * (thresh_multiplier + r), 255, THRESH_BINARY);
+	// Find contours
+	findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	Rect boundRect;
+	int biggestContour = GetMaxAreaContourId(contours);
+	if (biggestContour != -1) {
+		boundRect = boundingRect(Mat(contours[biggestContour]));
+		if (boundRect.area() > 10) {
+			Rect mapped_rect = Rect(initial_box.x + boundRect.x, initial_box.y + boundRect.y,
+				boundRect.width, boundRect.height);
+			rectangle(*output, mapped_rect, Scalar(0, 0, 255), 2, 8, 0);
+			Mat cropped = src->clone();
+			cropped = cropped(boundRect);
+			std::ostringstream oss;
+			oss << "Rec " << r;
+			namedWindow(oss.str(), WINDOW_FREERATIO);
+			imshow(oss.str(), cropped);
+			FindBiggestContour(&cropped, output, mapped_rect, r + 1);
+
+		}
+	}
+}
+
+Rect GetTopRect(std::vector<Rect> Rects)
+{
+	// Threshold
+	double maxY = 999999;
+	int topRectId = -1;
+	for (int j = 0; j < Rects.size(); j++) {
+		double y = Rects[j].y;
+		if (y < maxY) {
+			topRectId = j;
+		}
+	}
+	if (topRectId == -1)
+		return Rect(0, 0, 0, 0);
+	else
+		return Rects[topRectId];
+}
+
+void FindTopMostContour(const Mat * src, const Mat * output, Rect initial_box, int r) {
+	if ((initial_box.area() <= 10 && initial_box.area() != 0) || r >= 5) {
+		return;
+	}
+
+	std::vector<std::vector<Point>> contours;
+	std::vector<Vec4i> hierarchy;
+
+	Mat threshold_output;
+
+	// Detect edges using Threshold.
+	threshold(*src, threshold_output, thresh * (thresh_multiplier + r), 255, THRESH_BINARY);
+	// Find contours
+	findContours(threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+	std::vector<Rect> boundRects(contours.size());
+	for (int i = 0; i < contours.size(); i++) {
+		boundRects[i] = boundingRect(Mat(contours[i]));
+	}
+	Rect boundRect = GetTopRect(boundRects);
+
+
+	if (boundRect.area() > 10) {
+		Rect mapped_rect = Rect(initial_box.x + boundRect.x, initial_box.y + boundRect.y,
+			boundRect.width, boundRect.height);
+		rectangle(*output, mapped_rect, Scalar(0, 0, 255), 2, 8, 0);
+		Mat cropped = src->clone();
+		cropped = cropped(boundRect);
+		std::ostringstream oss;
+		oss << "Rec " << r;
+		namedWindow(oss.str(), WINDOW_FREERATIO);
+		imshow(oss.str(), cropped);
+		FindTopMostContour(&cropped, output, mapped_rect, r + 1);
+	}
+
+}
+
+int GetMaxAreaContourId(std::vector<std::vector<Point>> contours) {
+	// Threshold
+	double maxArea = 40;
+	int maxAreaContourId = -1;
+	for (int j = 0; j < contours.size(); j++) {
+		double newArea = contourArea(contours.at(j));
+		if (newArea > maxArea) {
+			maxArea = newArea;
+			maxAreaContourId = j;
+		}
+	}
+	return maxAreaContourId;
 }
 
 void PrintRealsenseError(const rs2::error & e) {
